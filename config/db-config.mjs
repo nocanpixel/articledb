b9514971f77ff7779252cba06dccef09348264a4
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-10-15 00:40:25 +0200

    db changed query

--- config/db-config.mjs
+++ config/db-config.mjs
@@ -10,23 +10,11 @@ export const sequelize = new Sequelize(
   {
     host: process.env.HOST,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true, // Require SSL connection
-        rejectUnauthorized: false, // Disable rejecting unauthorized connections (not recommended for production)
-      },
-    },
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true, // Require SSL connection
+    //     rejectUnauthorized: false, // Disable rejecting unauthorized connections (not recommended for production)
+    //   },
+    // },
   }
 );
-
-// Set the character set and collation at the database connection level
-sequelize
-  .query(
-    `ALTER DATABASE ${process.env.DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`
-  )
-  .then(() => {
-    console.log("Character set and collation set successfully.");
-  })
-  .catch((error) => {
-    console.error("Error setting character set and collation:", error);
-  });
