0e255bac722e3d6f8668c99f077cfa54d34d6fc3
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-10-16 09:55:15 +0200

    oRIGINs

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 
-const allowedOrigins = ['http://localhost:5173','https://cambe.app']
+const allowedOrigins = ['http://localhost:5173','https://www.cambe.app']
 
 app.use(cors({
   origin: allowedOrigins,
